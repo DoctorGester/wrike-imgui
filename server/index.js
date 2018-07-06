d27747ed9ce94bbf82e4b31063249a60a8e797fd
kirill.artemov <kirill.artyomov@team.wrike.com>
2018-07-06 17:05:43 +0300

    Added host parameter to server code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const request = require("request");
 const express = require("express");
 const app = express();
+const arguments = process.argv.slice(2);
+const host = arguments.length > 0 ? arguments[1] : 'localhost';
 
 app.use(express.static('out'));
 
@@ -8,4 +10,4 @@ app.get("/wrike/*", (req, res) => {
 	req.pipe(request(req.params[0])).pipe(res);
 });
 
-app.listen(3637, () => console.log("Server started"));
+app.listen(3637, host, () => console.log("Server started"));
