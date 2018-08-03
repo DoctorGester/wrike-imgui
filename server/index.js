d3e1204703da36f419d090bf5660967e97b19fff
kirill.artemov <kirill.artyomov@team.wrike.com>
2018-08-03 13:55:07 +0300

    API requests are now piped through backend to get rid of OPTIONS requests

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 const request = require("request");
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const arguments = process.argv.slice(2);
 const host = arguments.length > 0 ? arguments[1] : 'localhost';
+
+fs.readFile("private.key", "utf8", function(error, private_key) {
+    if (error) {
+        throw error;
+    }
+
+    const key_pattern = "bearer ";
+
+    private_key = private_key.substr(private_key.indexOf(key_pattern) + key_pattern.length);
+
+    app.get("/api/*", (req, res) => {
+        const new_request = request("https://www.wrike.com" + req.originalUrl, {
+            auth: {
+                bearer: private_key
+            }
+        });
+
+        req.pipe(new_request).pipe(res);
+    });
+});
 
 app.use(express.static('out'));
 
