334f47630cc38c8e3c45855a551f3f3a7aafcf15
kirill.artemov <kirill.artyomov@team.wrike.com>
2018-08-03 18:27:18 +0300

    HTTP2 support

--- server/index.js
+++ server/index.js
@@ -1,34 +1,40 @@
 const request = require("request");
 const express = require("express");
+const http2 = require('spdy');
 const fs = require("fs");
 const app = express();
 const arguments = process.argv.slice(2);
 const host = arguments.length > 0 ? arguments[1] : 'localhost';
 
-fs.readFile("private.key", "utf8", function(error, private_key) {
-    if (error) {
-        throw error;
-    }
+const options = {
+    key: fs.readFileSync("localhost.key", "utf8"),
+    cert: fs.readFileSync("localhost.crt", "utf8")
+};
 
-    const key_pattern = "bearer ";
-
-    private_key = private_key.substr(private_key.indexOf(key_pattern) + key_pattern.length);
-
-    app.get("/api/*", (req, res) => {
-        const new_request = request("https://www.wrike.com" + req.originalUrl, {
-            auth: {
-                bearer: private_key
-            }
-        });
-
-        req.pipe(new_request).pipe(res);
-    });
-});
+const key_pattern = "bearer ";
+const key_file = fs.readFileSync("private.key", "utf8");
+const private_key = key_file.substr(key_file.indexOf(key_pattern) + key_pattern.length);
 
 app.use(express.static('out'));
 
 app.get("/wrike/*", (req, res) => {
-	req.pipe(request(req.params[0])).pipe(res);
+    const new_request = request(req.params[0]).on('error', function(err) {
+        console.error(err);
+    });
+
+	req.pipe(new_request).pipe(res);
 });
 
-app.listen(3637, host, () => console.log("Server started"));
+app.get("/api/*", (req, res) => {
+    const new_request = request("https://www.wrike.com" + req.originalUrl, {
+        auth: {
+            bearer: private_key
+        }
+    }).on('error', function(err) {
+        console.error(err);
+    });
+
+    req.pipe(new_request).pipe(res);
+});
+
+http2.createServer(options, app).listen(3637, host, () => console.log("Server started"));
